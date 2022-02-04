afdcbe8c4e65ca94af00aeb8614c7ad7527ca478
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-02-04 04:47:24 -0600

    add html routes to index.js

---
+++ public/assets/js/idb.js
@@ -0,0 +1,35 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+const request = indexedDB.open('pizza_hunt', 1);
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    //save reference to database
+    const db = event.target.result;
+    //create an object store(table) called 'new_pizza', set it to have an auto incr primary key
+    db.createObjectStore('new_pizza', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.online) {
+        uploadPizza();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access the object store for `new_pizza`
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+    
+    //add record to store with add method
+    pizzaObjectStore.add(record);
+};
